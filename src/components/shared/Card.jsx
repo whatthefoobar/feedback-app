5056baadff901ec5c925dc0fc7635dcc47b5fb71
whatthefoobar <irina.mntn@gmail.com>
2022-03-31 16:22:26 +0200

    conditional classes/styling

---
+++ src/components/shared/Card.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+
+function Card({ children, reverse }) {
+  // return <div className={`card ${reverse && 'reverse'}`}>{children}</div> // conditional class
+
+  return (
+    <div
+      className="card"
+      style={{
+        backgroundColor: reverse ? 'rgba(0,0,0,0.4)' : '#fff',
+        color: reverse ? '#fff' : '#000',
+      }} // conditional styling
+    >
+      {children}
+    </div>
+  );
+}
+
+Card.defaultProps = {
+  reverse: false,
+};
+
+Card.propTypes = {
+  children: PropTypes.node.isRequired,
+  reverse: PropTypes.bool,
+};
+
+export default Card;
