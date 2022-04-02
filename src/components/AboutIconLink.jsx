93c97f5460c61d9567e2a41ed0dc292c5fd2511a
whatthefoobar <irina.mntn@gmail.com>
2022-04-02 13:02:16 +0200

    added context

--- src/components/AboutIconLink.jsx
+++ src/components/AboutIconLink.jsx
@@ -7,6 +7,7 @@ function AboutIconLink() {
       <Link to="/about">
         <FaQuestion size={30} />
       </Link>
+      {/* for any internal links use Link not a to avoid page refreshes */}
     </div>
   );
 }
