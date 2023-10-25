42f8d113ccbb3e4f0d08e012b715f1dbbe9d1523
whatthefoobar <irina.mntn@gmail.com>
2023-10-25 12:15:46 +0200

    publish

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,6 @@ import { errorHandler, notFound } from "./middleware/errorMidleware.js";
 import cors from "cors";
 
 const port = process.env.PORT || 5000;
-// console.log(process.env.MONGO_URI);
 connectDB();
 
 const app = express();
@@ -19,9 +18,18 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/feedbacks", FeedbackRoutes);
 
-app.get("/", (req, res) => {
-  res.send("API is running....");
-});
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  // if in production the frontend buikd is served from the published backend
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
